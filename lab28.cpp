d2a0bbdaba4068ad4c7ea80ba3d4713c0fadeb7c
Chevin Jeon <109643560+Chevinjeon@users.noreply.github.com>
2025-11-02 01:40:57 -0800

    third commit

--- lab28.cpp
+++ lab28.cpp
@@ -22,3 +22,15 @@ void find_goat(set<Goat> &trip);
 void count_goats_by_age(set<Goat> &trip);
 void find_if_old_goat(set<Goat> &trip);
 void for_each_display(set<Goat> &trip);
+void transform_goat_names(set<Goat> &trip);
+void accumulate_total_age(set<Goat> &trip);
+void remove_if_young_goat(set<Goat> &trip);
+void sort_by_age_display(set<Goat> &trip);
+
+int main() {
+    srand(time(0));
+    
+    // read & populate arrays for names and colors
+    ifstream fin("names.txt");
+    string names[SZ_NAMES];
+    int i = 0;
