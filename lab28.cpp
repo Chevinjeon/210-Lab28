463106b81d15fd54191cbc37c3dab3e67cc5ae87
Chevin Jeon <109643560+Chevinjeon@users.noreply.github.com>
2025-11-02 01:17:05 -0800

    first commit

---
+++ lab28.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <set>
+#include <algorithm>
+#include <numeric>
+#include <cstdlib>
+#include <ctime>
+#include "Goat.h"
+using namespace std;
